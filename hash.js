82f38e4853f2880c736ee0c5213bf06372c95d57
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-27 13:55:08 +0000

    Implement growth functionality when hash map reaches 0.75 capacity

--- hash.js
+++ hash.js
@@ -1,7 +1,7 @@
 import { LinkedList } from "./linkedList.js"
 
 class HashMap {
-  constructor(capacity, load) {
+  constructor(capacity = 16, load = 0.75) {
     this.buckets = []
     for (let i = 0; i < capacity; i++) {
       this.buckets.push([])
@@ -10,13 +10,13 @@ class HashMap {
     this.load = load
   }
 
-  hash(key) {
+  hash(key, capacity = this.capacity) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      hashCode = hashCode % this.capacity;
+      hashCode = hashCode % capacity;
     }
 
     return hashCode;
@@ -45,6 +45,11 @@ class HashMap {
         list.append(key, value);
       }
     }
+    if (this.length() > this.capacity * this.load) {
+      console.log('ENTERED');
+      console.log(this.length());
+      this.grow();
+    } 
   }
 
   get(key) {
@@ -160,13 +165,47 @@ class HashMap {
     }
     return entriesArr;
   }
+
+  grow() {
+    const newCapacity = this.capacity * 2;
+    const newArr = [];
+    for (let i = 0; i < (newCapacity); i++) {
+      newArr.push([]);
+    }
+    let currKeys = this.keys();
+
+    for (let key of currKeys) {
+      let bucket = newArr[this.hash(key, newCapacity)];
+      if (bucket.length === 0) {
+        const list = new LinkedList();
+        list.append(key, this.get(key));
+        bucket.push(list);
+      }
+      else {
+        const list = bucket[0];
+        list.append(key, this.get(key));
+      }
+    }
+    this.buckets = newArr;
+    this.capacity = newCapacity;
+  }
 }
 
-const test = new HashMap(8, 1);
-test.set('testr', 'value0');
-test.set('blabla', 'value1');
-test.set('tea', 'value2');
-test.set('bla','value3')
-setTimeout(() => {  
-  console.log(test.entries());
+const test = new HashMap();
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+console.log(test.buckets);
+setTimeout(() => {
+  test.set('moon', 'silver')
+  console.log(test.buckets);
 }, 0)
